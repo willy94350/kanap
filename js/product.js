7c5269edd4063340b7f91cfa249265e630455e72
willy94350 <wfrancillonne@yahoo.fr>
2023-02-13 18:33:22 +0100

    oui

--- js/product.js
+++ js/product.js
@@ -1,5 +1,5 @@
 const params=new URLSearchParams(window.location.search)
-
+let id = params.get("id");
 console.log(params.get("id"))
 
 fetch ('http://localhost:3000/api/products/'+params.get("id")) .then((response)=>response.json()).then((products)=>displayProducts(products))
@@ -41,13 +41,66 @@ for (let i=0;i<products.colors.length;i++){
 }
 
 document.getElementById('addToCart').addEventListener("click",()=>{
-    console.log("click");
+    
     //je récupère la couleur et la quantité//
-    //si jai une quantité et couleur correct alors je lajoute au panier sinon je préviens lutilisateur avec un
-   // message alert 
-   //jajoute le panier au storage
-    localStorage.setItem("object","bonjour" );
+   let color= document.getElementById('colors').value;
+   console.log(color);
+       //si jai une quantité et couleur correct alors je lajoute au panier sinon je préviens lutilisateur avec un
+     // message alert 
+   if (color === ""){
+    alert("veuillez mettre une couleur svp");
+
+   }
+   else {
+    alert("couleur séléctionné");
+    let quantity=document.getElementById('quantity').value;
+    if (quantity>0 && quantity<= 100){
+        alert("la quantité est séléctionné");
+    //ajoute le canapé au panier
+        let canape ={
+            id:"id",
+            quantity:"quantity",
+            couleur:"color"
+        }
+
+        let panier2 = JSON.parse(localStorage.getItem("object"));
+        console.log(panier2)
+    
+       //jajoute le panier au storage
+        localStorage.setItem ("object",JSON.stringify(canape));
+
+        if (panier2){panier2.push(canape)
+
+        }
+else {panier2=[];panier2.push(canape)}
+
+        }
+    
+    
+    
+      
+      //si le panier nexiste pas je le crée jajoute le canapé et je lajoute au storage
+      //sinon je traduit mon panier en javascript jajoute le canapé au panier et je met le panier au storage
+      
+    }
+
+    else{
+        alert("la quantité n'est pas correct");
+    }
+   }
+
+    
+
+
+
+
+    
+
 })
 
 
-
+   if (items.color=== ""){
+        alert("veuillez")
+    }
+    
+    
